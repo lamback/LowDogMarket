69683b5b354d393e8675bac99477a0c2b5e59d43
heizilanjin <547059872@qq.com>
2018-12-25 11:13:08 +0800

    修改

--- src/main/resources/static/js/hun.index.js
+++ src/main/resources/static/js/hun.index.js
@@ -75,33 +75,28 @@ window.onload = function () {
     getGoodsType();
     getGoodsByType();
 
-	var ul = document.getElementById ("piclist");
+    $.ajax({
+        type: "POST",//方法类型
+        dataType: "json",//预期服务器返回的数据类型
+        url: "/servlets/add" ,//url
+        success: function (result) {
+            var ul = document.getElementById ("piclist");
 
-	//for(let a=0;a<10;a++) ul.innerHTML+="<li></li>"
-    var lilist=ul.getElementsByTagName("li");
+            //for(let a=0;a<10;a++) ul.innerHTML+="<li></li>"
+            var lilist=ul.getElementsByTagName("li");
+            for (var i = 0; i < lilist.length; i++) {          //生成排行榜
+                var photo="",information="",goodsid="";
+                lilist[i].innerHTML="<i>"+(i+1)+"</i>";
+                lilist[i].innerHTML+="<div class='pic f1'>";
+                console.log(result);
+                lilist[i].innerHTML+="<a href='#'><img src="+result[i].photo+"></a></div>"+"<div class='title w-all fl m-t-18'><a href='//localhost:8080/good.html?goodsid="+result[i].goodsid+"' class='dsblock w-all fl'> ";
+                lilist[i].innerHTML+="<h3 class='fs-14 ftc-787878 text-l line-h18 tw_hidden fl'>"+result[i].information+"</h3></a></div>";
 
-
-    for (var i = 0; i < lilist.length; i++) {          //生成排行榜
-    	var photo,information,goodsid;
-        lilist[i].innerHTML="<i>"+(i+1)+"</i>";
-        lilist[i].innerHTML+="<div class='pic f1'><a href='#'>";
-        $.ajax({
-            type: "POST",//方法类型
-            dataType: "json",//预期服务器返回的数据类型
-            url: "/salesRanking" ,//url
-            success: function (result) {
-					photo=result[i].photo;
-					information=result[i].information;
-					goodsid=result[i].goodsid;
             }
-        })
-        lilist[i].innerHTML+="<img src="+photo+"/></a></div>"+"<div class='title w-all fl m-t-18'><a href='//localhost:8080/good.html?goodsid="+goodsid+"' class='dsblock w-all fl'> ";
-        lilist[i].innerHTML+="<h3 class='fs-14 ftc-787878 text-l line-h18 tw_hidden fl'>"+information+"</h3></a></div>";
+        }
+    });
 
-
-    };
-
-
+    var ul = document.getElementById ("piclist");
     var lis = ul.getElementsByTagName("i");
 
 	lis[0].className="picspan_icon1";
