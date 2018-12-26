2d81f7c3df77ac736e441a3f6a773d4972e89d27
Charles1909 <44265480+Charles1909@users.noreply.github.com>
2018-12-26 19:13:38 +0800

    凄凄切切

--- src/main/resources/static/js/orderJS.js
+++ src/main/resources/static/js/orderJS.js
@@ -1,4 +1,33 @@
+window.onload=function ()
+{
+    var loginText=document.getElementById("loginText");
 
+
+    $.ajax({
+        type: "POST",//方法类型
+        dataType: "json",//预期服务器返回的数据类型
+        url: "/judgeUser" ,//url
+
+        success: function (result) {
+            if(result.status==1)
+            {
+                loginText.innerHTML+="<span class='fs-14 line-h30 ftc-6b6b6b dsblock fl'>Hello!"+result.username+"</span>";
+            }
+            else
+            {   window.location.href="../login.html";     //未登录则跳转到登录界面
+                loginText.innerHTML+="<a href='#' class='dsblock fl line-h30 fs-14 ftc-e23435 m-l-40'>Hi，请登录</a>";
+                loginText.innerHTML+="<a href='#' class='dsblock fl line-h30 fs-14 ftc-e23435 m-l-40'>注册</a>";
+            }
+        },
+        error:function () {
+            window.location.href="../login.html";         //未登录则跳转到登录界面
+            loginText.innerHTML+="<a href='#' class='dsblock fl line-h30 fs-14 ftc-e23435 m-l-40'>Hi，请登录</a>";
+            loginText.innerHTML+="<a href='#' class='dsblock fl line-h30 fs-14 ftc-e23435 m-l-40'>注册</a>";
+        }
+
+    })
+    onOrderReady();
+};
 
 //请求服务器端获取数据，并存到resultJSON中
 function requestJSON(){
